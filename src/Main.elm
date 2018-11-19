cde8e09d37b1952300bd21d25619f772248bdf4a
Pablo <pablo.bollansee@gmail.com>
2018-11-19 11:09:58 +0100

    Automatic elm building

--- src/Main.elm
+++ src/Main.elm
@@ -2,22 +2,35 @@ import Browser
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
 
-main =
-  Browser.sandbox { init = 0, update = update, view = view }
+type alias Model = (Int, Int)
 
-type Msg = Increment | Decrement
+type Msg = ChangeFirst Int | ChangeSecond Int
 
-update msg model =
-  case msg of
-    Increment ->
-      model + 1
+init : Model
+init = (0, 0)
 
-    Decrement ->
-      model - 1
+main = Browser.sandbox { init = init, update = update, view = view }
 
-view model =
-  div []
-    [ button [ onClick Decrement ] [ text "-" ]
-    , div [] [ text (String.fromInt model) ]
-    , button [ onClick Increment ] [ text "+" ]
-    ]
+
+update : Msg -> Model -> Model
+update msg (first, second) =
+    case msg of
+        ChangeFirst i -> (first + i, second)
+
+        ChangeSecond i -> (first, second + i)
+
+
+view : Model -> Html Msg
+view (first, second) =
+    div []
+        [ div []
+            [ button [ onClick <| ChangeFirst -1 ] [ text "-" ]
+            , div [] [ text (String.fromInt first) ]
+            , button [ onClick <| ChangeFirst 1 ] [ text "+" ]
+            ]
+        , div []
+            [ button [ onClick <| ChangeSecond -1 ] [ text "-" ]
+            , div [] [ text (String.fromInt second) ]
+            , button [ onClick <| ChangeSecond 1 ] [ text "+" ]
+            ]
+        ]
