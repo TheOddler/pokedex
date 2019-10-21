433d41aecdfb4e9667ea31335b430cc00581a6af
Pablo Bollansée <pablo.bollansee@gmail.com>
2019-10-21 12:27:37 +0200

    Added hot tag

--- startlive.bat
+++ startlive.bat
@@ -1,2 +1,2 @@
 :: Elm Live: https://github.com/wking-io/elm-live
-elm-live src/Main.elm --open --dir=src --start-page=index.html -- --output=src/elm.js --debug
+elm-live src/Main.elm --open --hot --dir=src --start-page=index.html -- --output=src/elm.js --debug
