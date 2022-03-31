143aadf563985c0225de59e686c9db3d36e87548
Pablo Bollansee <pablo.bollansee@gmail.com>
2022-03-31 10:02:47 +0100

    Added links to mega Pokemon in the evolutions view

--- src/Pokemon.elm
+++ src/Pokemon.elm
@@ -284,9 +284,9 @@ fromCSVRow pkmCSVRows pkm =
     , superEffective = List.filter isSuperEffective effectivenessList
     , notVeryEffective = List.filter isNotVeryEffective effectivenessList
     , imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/" ++ pkm.image ++ ".png"
-    , evolvesFromID = pkm.evolvesFromID
+    , evolvesFromID = Maybe.or pkm.evolvesFromID pkm.originalPokemonID
     , evolvesFromDetails = pkm.evolutionDetails
-    , evolvesIntoIDs = List.map .id <| List.filter (\p -> p.evolvesFromID == Just pkm.id) pkmCSVRows
+    , evolvesIntoIDs = List.map .id <| List.filter (\p -> p.evolvesFromID == Just pkm.id || p.originalPokemonID == Just pkm.id) pkmCSVRows
     }
 
 
