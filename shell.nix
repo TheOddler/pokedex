e01bbdd37509b5f2dec371bc53ce1dc7fc662410
Pablo Bollansée <pablo.bollansee@gmail.com>
2022-12-12 09:07:29 +0000

    Added elm-analyse to dev shell

--- shell.nix
+++ shell.nix
@@ -6,6 +6,7 @@ mkShell {
     elmPackages.elm-format # formatter
     elmPackages.elm-live # a live server for development
     elmPackages.elm-json # Install, upgrade and uninstall Elm dependencies
+    elmPackages.elm-analyse # Analyses your code and give improvement suggestions
     nodePackages.uglify-js # Further optimization of output on build
   ];
 }
