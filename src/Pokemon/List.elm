e4443d09901fd76f14eec66bf3b517a94b72516b
Pablo Bollansée <pablo.bollansee@gmail.com>
2023-01-27 03:09:42 +0000

    Removed wrapper from pokedex list
    This was only needed for the lazy, and with keyed it doesn't seem to be needed any more really.
    It also reduces the number of dom elements, so that actually improves performance.

--- src/Pokemon/List.elm
+++ src/Pokemon/List.elm
@@ -4,7 +4,6 @@ import Helpers exposing (stopPropagationOnClick)
 import Html exposing (Html, div, img, text)
 import Html.Attributes exposing (alt, attribute, class, src)
 import Html.Keyed as Keyed
-import Html.Lazy as Lazy
 import Pokemon exposing (Pokemon)
 import Pokemon.Details
 import Type
@@ -13,29 +12,21 @@ import Type
 view : List Pokemon -> (Pokemon -> Bool) -> Html Pokemon.Details.Msg
 view pokemonList filter =
     Keyed.node "div" [ class "list" ] <|
-        List.map (\p -> ( String.fromInt p.id, viewListElementWrapper (filter p) p )) pokemonList
+        List.map (\p -> ( String.fromInt p.id, viewListElement (filter p) p )) pokemonList
 
 
-viewListElementWrapper : Bool -> Pokemon -> Html Pokemon.Details.Msg
-viewListElementWrapper isVisible pkm =
-    div
-        [ class "itemWrapper" -- so the inner-item can be lazy
-        , if isVisible then
-            class "visible"
-
-          else
-            class "hidden"
-        ]
-        [ Lazy.lazy viewListElement pkm ]
-
-
-viewListElement : Pokemon -> Html Pokemon.Details.Msg
-viewListElement pkm =
+viewListElement : Bool -> Pokemon -> Html Pokemon.Details.Msg
+viewListElement isVisible pkm =
     div
         [ stopPropagationOnClick <| Pokemon.Details.Select pkm
         , Type.backgroundFor pkm.typing
         , class "item"
         , class "pokemonBadge"
+        , if isVisible then
+            class "visible"
+
+          else
+            class "hidden"
         ]
         [ img
             [ src pkm.imageUrl
